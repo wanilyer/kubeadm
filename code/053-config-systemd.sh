9ae01c39c94fef740031bdc27f9cc0f9522ce72e
wanilyer <50444221+wanilyer@users.noreply.github.com>
2019-11-11 17:45:10 +0800

    fix

--- code/053-config-systemd.sh
+++ code/053-config-systemd.sh
@@ -18,21 +18,10 @@ EOF
 
 # 配置kubelet启动参数: KUBELET_EXTRA_ARGS
 cat << EOF > /etc/sysconfig/kubelet 
-KUBELET_EXTRA_ARGS=--pod-infra-container-image=registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1
+KUBELET_EXTRA_ARGS=--pod-infra-container-image=registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1 --cgroup-driver=systemd
 EOF
 
-# 配置 kubelet cgroup driver
-# EOF上的单引号表示输出到文件时，不替换其中的变量
-# kubelet启动时会去/etc/sysconfig/kubelet文件中查找KUBELET_EXTRA_ARGS变量的值
-cat << 'EOF' > /etc/systemd/system/kubelet.service.d/20-etcd-service-manager.conf
-[Service]
-ExecStart=
-#  Replace "systemd" with the cgroup driver of your container runtime. The default value in the kubelet is "cgroupfs".
-ExecStart=/usr/bin/kubelet --address=127.0.0.1 --pod-manifest-path=/etc/kubernetes/manifests --cgroup-driver=systemd $KUBELET_EXTRA_ARGS
-Restart=always
-EOF
-
-systemctl restart docker
 systemctl daemon-reload
+systemctl restart docker
 systemctl restart kubelet
 
